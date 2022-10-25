e6640b7f475109ceb177d5e083ecd5318c38f86a
Bobertkiller <felipe.mb142@gmail.com>
2022-10-25 19:38:56 -0300

    new algorithm soon to be more

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -32,3 +32,14 @@ def selection(arr):
 print(selection(a))
 
 #insertion sort
+def insertion(arr):
+    for i in range(1,len(arr)):
+        x = arr[i]
+        j = i-1
+        while j>= 0 and x < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = x
+    return arr
+
+print(insertion(a))
